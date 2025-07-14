37bca42f172228c564afd9d359b26d29d2d28060
Chisom Eze <ezechisom966@gmail.com>
2025-07-14 18:18:55 +0100

    new files added

--- basic_operations.py
+++ basic_operations.py
@@ -1,11 +1,7 @@
 number1 = 10
 number2 = 5
-print(number1 + number2)
-print("Addition of 10 and 5 is 15")
 
-print(number1 - number2)
-print("Subtraction of 10 and 5 is 5")
-
-print(number1 * number2)
-print("Multiplication of 10 and 5 is 50")
+print("Addition of 10 and 5 is", number1 + number2)
+print("Subtraction of 10 and 5 is", number1 - number2)
+print("Multiplication of 10 and 5 ", number1 * number2)
 
