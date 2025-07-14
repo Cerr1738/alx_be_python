4d43fcdd2d9681992fddaf2164857cba1360b523
Chisom Eze <ezechisom966@gmail.com>
2025-07-14 15:10:17 +0100

    first commit

---
+++ basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+print(number1 + number2)
+print("Addition of 10 and 5 is 15")
+
+print(number1 - number2)
+print("Subtraction of 10 and 5 is 5")
+
+print(number1 * number2)
+print("Multiplication of 10 and 5 is 50")
+
